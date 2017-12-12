f42d2b0850726ff491661f15ac7968b039c0a798
rupeshbhatti <rupesh.bhatti@hotmail.com>
2017-12-12 21:49:27 +0000

    Fixed age slider bug with blue line added by chrome

--- src/components/prescreener/RiskFactors.js
+++ src/components/prescreener/RiskFactors.js
@@ -10,41 +10,51 @@ const RiskFactors = ({ handleRiskFactorRadio, switchVisibleComponent }) => {
         <form>
           <h1>Select the appropriate options below</h1>
           <div className="questionContent">
-            <h2>I am overweight</h2>
-            <input type="radio" name="obese" id="p_7" value="present" onChange={ handleRiskFactorRadio } required/>
-            <label>Yes</label><br />
-            <input type="radio" name="obese" id="p_7" value="absent" onChange={ handleRiskFactorRadio } required/>
-            <label>No</label><br />
-            <input type="radio" name="obese" id="p_7" value="unknown" onChange={ handleRiskFactorRadio } required />
-            <label>Unsure</label><br />
-            <h2>I smoke cigarettes</h2>
-            <input type="radio" name="smoker" id="p_28" value="present" onChange={ handleRiskFactorRadio } required/>
-            <label>Yes</label><br />
-            <input type="radio" name="smoker" id="p_28" value="absent" onChange={ handleRiskFactorRadio } required/>
-            <label>No</label><br />
-            <input type="radio" name="smoker" id="p_28" value="unknown" onChange={ handleRiskFactorRadio } required/>
-            <label>Unsure</label><br />
-            <h2>I have cholesterol</h2>
-            <input type="radio" name="cholesterol" id="p_10" value="present" onChange={ handleRiskFactorRadio } required/>
-            <label>Yes</label><br />
-            <input type="radio" name="cholesterol" id="p_10" value="absent" onChange={ handleRiskFactorRadio } required/>
-            <label>No</label><br />
-            <input type="radio" name="cholesterol" id="p_10" value="unknown" onChange={ handleRiskFactorRadio } required/>
-            <label>Unsure</label><br />
-            <h2>I have hypertension</h2>
-            <input type="radio" name="hypertension" id="p_9" value="present" onChange={ handleRiskFactorRadio } required/>
-            <label>Yes</label><br />
-            <input type="radio" name="hypertension" id="p_9" value="absent" onChange={ handleRiskFactorRadio } required/>
-            <label>No</label><br />
-            <input type="radio" name="hypertension" id="p_9" value="unknown" onChange={ handleRiskFactorRadio } required/>
-            <label>Unsure</label><br />
-            <h2>I am diabetic</h2>
-            <input type="radio" name="diabetic" id="p_8" value="present" onChange={ handleRiskFactorRadio } required/>
-            <label>Yes</label><br />
-            <input type="radio" name="diabetic" id="p_8" value="absent" onChange={ handleRiskFactorRadio } required/>
-            <label>No</label><br />
-            <input type="radio" name="diabetic" id="p_8" value="unknown" onChange={ handleRiskFactorRadio } required/>
-            <label>Unsure</label><br />
+            <div className="questionContainer">
+              <h2>I am overweight</h2>
+              <input type="radio" name="obese" id="p_7" value="present" onChange={ handleRiskFactorRadio } required/>
+              <label>Yes</label><br />
+              <input type="radio" name="obese" id="p_7" value="absent" onChange={ handleRiskFactorRadio } required/>
+              <label>No</label><br />
+              <input type="radio" name="obese" id="p_7" value="unknown" onChange={ handleRiskFactorRadio } required />
+              <label>Unsure</label><br />
+            </div>
+            <div className="questionContainer">
+              <h2>I smoke cigarettes</h2>
+              <input type="radio" name="smoker" id="p_28" value="present" onChange={ handleRiskFactorRadio } required/>
+              <label>Yes</label><br />
+              <input type="radio" name="smoker" id="p_28" value="absent" onChange={ handleRiskFactorRadio } required/>
+              <label>No</label><br />
+              <input type="radio" name="smoker" id="p_28" value="unknown" onChange={ handleRiskFactorRadio } required/>
+              <label>Unsure</label><br />
+            </div>
+            <div className="questionContainer">
+              <h2>I have cholesterol</h2>
+              <input type="radio" name="cholesterol" id="p_10" value="present" onChange={ handleRiskFactorRadio } required/>
+              <label>Yes</label><br />
+              <input type="radio" name="cholesterol" id="p_10" value="absent" onChange={ handleRiskFactorRadio } required/>
+              <label>No</label><br />
+              <input type="radio" name="cholesterol" id="p_10" value="unknown" onChange={ handleRiskFactorRadio } required/>
+              <label>Unsure</label><br />
+            </div>
+            <div className="questionContainer">
+              <h2>I have hypertension</h2>
+              <input type="radio" name="hypertension" id="p_9" value="present" onChange={ handleRiskFactorRadio } required/>
+              <label>Yes</label><br />
+              <input type="radio" name="hypertension" id="p_9" value="absent" onChange={ handleRiskFactorRadio } required/>
+              <label>No</label><br />
+              <input type="radio" name="hypertension" id="p_9" value="unknown" onChange={ handleRiskFactorRadio } required/>
+              <label>Unsure</label><br />
+            </div>
+            <div className="questionContainer">
+              <h2>I am diabetic</h2>
+              <input type="radio" name="diabetic" id="p_8" value="present" onChange={ handleRiskFactorRadio } required/>
+              <label>Yes</label><br />
+              <input type="radio" name="diabetic" id="p_8" value="absent" onChange={ handleRiskFactorRadio } required/>
+              <label>No</label><br />
+              <input type="radio" name="diabetic" id="p_8" value="unknown" onChange={ handleRiskFactorRadio } required/>
+              <label>Unsure</label><br />
+            </div>
             <br />
           </div>
           <hr />
