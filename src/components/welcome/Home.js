6c90a8b5592eb250fbd82ec197dde118f0a3d0f1
rupeshbhatti <rupesh.bhatti@hotmail.com>
2017-12-12 20:19:05 +0000

    Finished styling home

--- src/components/welcome/Home.js
+++ src/components/welcome/Home.js
@@ -8,6 +8,9 @@ const Home = ({switchVisibleComponent}) => {
     <div id="home">
       <form>
         <img src="/assets/DrWebberLogo.png" />
+        <div id="strapline">
+          <label className="strap">The future of healthcare is here.</label>
+        </div>
         <button className="homebutton" onClick={switchVisibleComponent} value="PatientGender">Begin</button>
         <div className="arrow bounce"></div>
       </form>
